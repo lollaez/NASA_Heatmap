732d3b8f3f38b0584761833cf13e0432eebd102b
lollaez <ellianaz_ez@hotmail.com>
2025-07-30 12:00:05 -0700

    included safety area for linear and single

--- linear.py
+++ linear.py
@@ -4,9 +4,10 @@ VERTIPORT_SIDE = 48 * 3 #ft archer midnight - safety area = 3 times wingspan
 #SAFTEY_SIDE = 25 #ft <- include in the future
 GATE_SIDE = 48 * 1.5 #ft wingspan 48ft <- change to larger porportion (just driving)
 TAKELAND_TIME = 2 #min
+SAFETY_SIDE = 48
 
-areaWidth = 517 #ft
-areaLength = 600 #ft
+areaWidth = 200 #ft
+areaLength = 502 #ft
 
 gatesToPad = 0 #ratio of pads to each gate
 maybeDouble = False #double # of vertiports & gates
@@ -17,10 +18,54 @@ cycleTime = 0 #min
 
 #>>>>>check if long/wide enough for this config
 def check_area_linear(width, length):
+    #safety needed means if needs extra area to fit
+    safety_needed = False
+    large_enough = False
+    max_gate_ratio = 0
+    #check for both gate ratios
     if width >= (VERTIPORT_SIDE + GATE_SIDE) and length >= (VERTIPORT_SIDE + GATE_SIDE):
-        return(True)
+        print("Large Enough With Safety Area & Gate Ratio of 3")
+        safety_needed = False
+        large_enough = True
+        large_enough = True
+        max_gate_ratio = 3
+    elif width >= (VERTIPORT_SIDE + GATE_SIDE) or length >= (VERTIPORT_SIDE + GATE_SIDE):
+        if width >= VERTIPORT_SIDE and length >= VERTIPORT_SIDE:
+            print("Large Enough With Safety Area & Gate Ratio of 2")
+            safety_needed = False
+            large_enough = True
+            max_gate_ratio = 2
+    #check if large enough without safety
+    elif (width + SAFETY_SIDE) >= (VERTIPORT_SIDE + GATE_SIDE) and (length + SAFETY_SIDE) >= (VERTIPORT_SIDE + GATE_SIDE):
+        if width >= (VERTIPORT_SIDE + GATE_SIDE):
+            print("Large Enough Without Safety Area & Gate Ratio of 3 - Length Too Short")
+            safety_needed = True 
+            large_enough = True
+            max_gate_ratio = 3
+        elif length >= (VERTIPORT_SIDE + GATE_SIDE):
+            print("Large Enough Without Safety Area & Gate Ratio of 3 - Width Too Short")
+            safety_needed = True
+            large_enough = True
+            max_gate_ratio = 3
+        else:
+            print("Large Enough Without Safety Area & Gate Ratio of 3 - Both Too Short")
+            safety_needed = True
+            large_enough = True
+            max_gate_ratio = 3
+    elif (width + SAFETY_SIDE) >= (VERTIPORT_SIDE + GATE_SIDE) or (length + SAFETY_SIDE) >= (VERTIPORT_SIDE + GATE_SIDE):
+        if width >= VERTIPORT_SIDE and length >= VERTIPORT_SIDE:
+            print("Large Enough Without Safety Area & Gate Ratio of 2 - Longer Side Too Short")
+            safety_needed = True
+            large_enough = True
+            max_gate_ratio = 2
+        elif (width + SAFETY_SIDE) >= VERTIPORT_SIDE and (length + SAFETY_SIDE) >= VERTIPORT_SIDE:
+            print("Large Enough Without Safety Area & Gate Ratio of 2 - Both Too Short")
+            safety_needed = True
+            large_enough = True
+            max_gate_ratio = 2
     else:
-        return(False)
+        large_enough = False
+    return(large_enough, safety_needed, max_gate_ratio)
 
 #if width is longer, this function returns "True"
 def findLongerSide(width, length):
@@ -44,15 +89,48 @@ def checkMaybeDouble(width, length):
 
 #find number of vertiports based on gateToPadRatio
 def checkVertiportsLinear(width, length):
-    if findLongerSide(width, length) == True:
-        totalGates = math.floor(width/GATE_SIDE)
-        vertiports3 = math.floor(totalGates/3)
-        vertiports2 = math.floor(totalGates/2)
+    large_enough, safety_needed, max_gate_ratio = check_area_linear(areaWidth, areaLength)
+    #Max Gate Ratio of 2 Without Safety
+    if max_gate_ratio == 2 and safety_needed == True:
+        vertiports3 = 0
+        vertiports2 = 0
+        vertiports3safety = 0
+        vertiports2safety = 1
+    #Max Gate Ratio of 2 With Safety
+    elif max_gate_ratio == 2 and safety_needed == False:
+        vertiports3 = 0
+        vertiports2 = 1
+        vertiports3safety = 0
+        vertiports2safety = 1
+    #Max Gate Ratio of 3 Without Safety
+    elif max_gate_ratio == 3 and safety_needed == True:
+        vertiports3 = 0
+        vertiports2 = 1 #(144 + 72 - 48)/(144)
+        vertiports3safety = 1
+        vertiports2safety = 1 #(144 + 72)/(144)
+    #Max Gate Ratio of 3 With Safety
+    elif max_gate_ratio == 3 and safety_needed == False:
+        vertiports3 = 1
+        vertiports2 = 1 #(144 + 72)/(144)
+        vertiports3safety = 1
+        vertiports2safety = 1 #(144 + 72 + 48)/(144)
+    #Regular
     else:
-        totalGates = math.floor(length/GATE_SIDE)
-        vertiports3 = math.floor(totalGates/3)
-        vertiports2 = math.floor(totalGates/2)
-    return(vertiports3, vertiports2)
+        if findLongerSide(width, length) == True:
+            totalGates = math.floor(width/GATE_SIDE)
+            vertiports3 = math.floor(totalGates/3)
+            vertiports2 = math.floor(totalGates/2)
+            totalGatesSafety = math.floor((width + SAFETY_SIDE)/GATE_SIDE)
+            vertiports3safety = math.floor(totalGatesSafety/3)
+            vertiports2safety = math.floor(totalGatesSafety/2)
+        else:
+            totalGates = math.floor(length/GATE_SIDE)
+            vertiports3 = math.floor(totalGates/3)
+            vertiports2 = math.floor(totalGates/2)
+            totalGatesSafety = math.floor((length + SAFETY_SIDE)/GATE_SIDE)
+            vertiports3safety = math.floor(totalGatesSafety/3)
+            vertiports2safety = math.floor(totalGatesSafety/2)
+    return(vertiports3, vertiports2, vertiports3safety, vertiports2safety)
 
 #print out throughput (flights per hour) for every gateToPadRatio & list of gateToPadRatios and waitTimes
 def calculateFlights(width, length, vertiports, cycleTime):
@@ -77,16 +155,22 @@ if findLongerSide(areaWidth, areaLength) == True:
 else:
     print("Longer Side: Length")
 
-if check_area_linear(areaWidth, areaLength) == True:
+large_enough, safety_needed, max_gate_ratio = check_area_linear(areaWidth, areaLength)
+
+if large_enough == True:
     maybeDouble = checkMaybeDouble(areaWidth, areaLength)
     print("Short Side Long Enough To Double: " + str(maybeDouble))
 
-    vertiports3, vertiports2 = checkVertiportsLinear(areaWidth, areaLength)
+    vertiports3, vertiports2, vertiports3safety, vertiports2safety = checkVertiportsLinear(areaWidth, areaLength)
     print(f"Vertiports With Ratio 3: {vertiports3}, Vertiports With Ratio 2: {vertiports2}")
+    print(f"(Without Safety) Vertiports With Ratio 3: {vertiports3safety}, Vertiports With Ratio 2: {vertiports2safety}")
 
     numFlightsLinear3 = calculateFlights(areaWidth, areaLength, vertiports3, 4.66)
     numFlightsLinear2 = calculateFlights(areaWidth, areaLength, vertiports2, 5)
+    numFlightsLinear3Safety = calculateFlights(areaWidth, areaLength, vertiports3safety, 4.66)
+    numFlightsLinear2Safety = calculateFlights(areaWidth, areaLength, vertiports2safety, 5)
     print(f"Flights With Ratio 3: {numFlightsLinear3} Flights With Ratio 2: {numFlightsLinear2}")
+    print(f"(Without Safety) Flights With Ratio 3: {numFlightsLinear3Safety} Flights With Ratio 2: {numFlightsLinear2Safety}")
     
 else:
     print("Area Not Large Enough for Linear")
--- single.py
+++ single.py
@@ -1,13 +1,7 @@
 import math
 
-#change to be able to import table of places & export same table but with data on different configurations
-#output a graph of different configurations and their throughput - build upon other research paper
-#determines which topology is best for a space depending on - size, width/length ratio,
-#Include different configurations
-#Include travel time between different parts
-
-area_width = 517 #ft
-area_length = 600 #ft
+area_width = 121 #ft
+area_length = 502 #ft
 
 flight_time = 10 #min
 
@@ -19,28 +13,48 @@ GATE_SIDE = 72
 
 #checking that the given area is big enough for a vertiport
 def check_area_single(width, length):
+    safety_needed = False
+    large_enough = False
     if width >= VERTIPORT_SIDE and length >= VERTIPORT_SIDE:
         print("Large Enough With Safety Area")
-        return(True)
+        safety_needed = False
+        large_enough = True
     elif width >= (VERTIPORT_SIDE - SAFETY_SIDE) and length >= (VERTIPORT_SIDE - SAFETY_SIDE):
         print("Large Enough Without Safety Area")
         if width < VERTIPORT_SIDE:
             print("Width Is Too Short If Safety Area Needed")
+            safety_needed = True
+            large_enough = True
         else:
             print("Length Is Too Short If Safety Area Needed")
-        return(True)
+            safety_needed = True
+            large_enough = True
     else:
-        return(False)
+        safety_needed = False
+        large_enough = False
+    return(large_enough, safety_needed)
 
 #checks if short side is long enough to double, can only double because it wouldn't make sense otherwise
 def double_single(width, length):
     if width >= length:
-        if width >= 2 * VERTIPORT_SIDE:
+        if length >= 2 * VERTIPORT_SIDE:
             return(True)
         else:
             return(False)
     else:
-        if length >= 2 * VERTIPORT_SIDE:
+        if width >= 2 * VERTIPORT_SIDE:
+            return(True)
+        else:
+            return(False)
+        
+def double_single_safety(width, length):
+    if width >= length:
+        if (length + SAFETY_SIDE) >= 2 * VERTIPORT_SIDE:
+            return(True)
+        else:
+            return(False)
+    else:
+        if (width + SAFETY_SIDE) >= 2 * VERTIPORT_SIDE:
             return(True)
         else:
             return(False)
@@ -48,23 +62,43 @@ def double_single(width, length):
 #calculate number of vertiports with one area for landing, dropoff, & takeoff
 def calculate_vertiports_single(width, length):
     #find the longer side
-    if width >= length:
-    #Make ony 100 x 100 ft boxes of vertiports and not include excess areas
-        number_width = math.floor(width/VERTIPORT_SIDE)
-        #double?
-        if double_single(width, length) == True:
-            print("Long Enough To Double")
-            number_vertiports = number_width * 2
-        else:
-            number_vertiports = number_width
+    if double_single(width, length) == True:
+        print("Long Enough To Double")
+        double = 2    
     else:
-        number_length = math.floor(length/VERTIPORT_SIDE)
-        if double_single(width, length) == True:
-            print("Long Enough To Double")
-            number_vertiports = number_length * 2
+        double = 1
+    
+    if double_single_safety(width, length) == True:
+        print("Long Enough To Double Without Safety")
+        doubleSafety = 2    
+    else:
+        doubleSafety = 1
+
+    large_enough, safety_needed = check_area_single(area_width, area_length)
+
+    if width >= length:
+        if safety_needed == False:
+            number_width = math.floor(width/VERTIPORT_SIDE)
+            number_vertiports = number_width * double
+            number_width_safety = math.floor((width + SAFETY_SIDE)/VERTIPORT_SIDE)
+            number_vertiports_safety = number_width_safety * doubleSafety
         else:
-            number_vertiports = number_length
-    return(number_vertiports)
+            number_vertiports = 0
+            number_width_safety = math.floor((width + SAFETY_SIDE)/VERTIPORT_SIDE)
+            number_vertiports_safety = number_width_safety * doubleSafety
+    else:
+        if safety_needed == False:
+            number_length = math.floor(length/VERTIPORT_SIDE)
+            number_vertiports = number_length * double
+            number_length_safety = math.floor((length + SAFETY_SIDE)/VERTIPORT_SIDE)
+            number_vertiports_safety = number_length_safety * doubleSafety
+
+        else:
+            number_vertiports = 0
+            number_length_safety = math.floor((length + SAFETY_SIDE)/VERTIPORT_SIDE)
+            number_vertiports_safety = number_length_safety * doubleSafety
+
+    return(number_vertiports, number_vertiports_safety)
     
 #calculates flights per hour based on minutes per flight
 def calculate_flights(vertiports, time):
@@ -72,12 +106,16 @@ def calculate_flights(vertiports, time):
     return(flights)
 
 print(f"Area Width: {area_width}, Area Length: {area_length}")
+large_enough, safety_needed = check_area_single(area_width, area_length)
 
-if check_area_single(area_width, area_length) == True:
-    vertiports = calculate_vertiports_single(area_width, area_length)
+if large_enough == True:
+    vertiports, vertiports_safety = calculate_vertiports_single(area_width, area_length)
     flights_single = calculate_flights(vertiports, flight_time)
+    flights_single_safety = calculate_flights(vertiports_safety, flight_time)
     
     print( "Number of Vertiports (Single Config): " + str(vertiports))
-    print("Number of Flights Per Hour (Single Config): " + str(flights_single))
+    print("Flights Per Hour (Single Config): " + str(flights_single))
+    print( "(Without Safety) Number of Vertiports (Single Config): " + str(vertiports_safety))
+    print("(Without Safety) Flights Per Hour (Single Config): " + str(flights_single_safety))
 else:
     print("Area Not Large Enough For Single")
